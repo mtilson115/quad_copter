9911d2a7bf050820cbcdf074113355d500f57fec
mtilson115 <mtilson115@gmail.com>
2016-01-24 23:55:16 -0800

    Fix tabs

--- drivers/inc/driver_uart.h
+++ drivers/inc/driver_uart.h
@@ -16,53 +16,53 @@ extern "C" {
 /*******************************************************************************
  * Includes
  ******************************************************************************/
-    #include "type_defs.h"
-    #include "perif_sfr_map.h"
+#include "type_defs.h"
+#include "perif_sfr_map.h"
 
 /*******************************************************************************
  * Defines
  ******************************************************************************/
-    #define UART_PERIF_NUM 2
+#define UART_PERIF_NUM 2
 
 /*******************************************************************************
  * Type Defs
  ******************************************************************************/
-    typedef struct {
-        BOOL    enabled;
-        int32_t     baud;
-        int32_t     data_bits;
-        int32_t     parity;
-        int32_t     stop_bits;
-    } UART_ctrl_type;
+typedef struct {
+    BOOL    enabled;
+    int32_t     baud;
+    int32_t     data_bits;
+    int32_t     parity;
+    int32_t     stop_bits;
+} UART_ctrl_type;
 
-    typedef struct
-    {
-	volatile REG_SET    mode;
-	volatile REG_SET    sta;
-	volatile REG_SET    tx;
-	volatile REG_SET    rx;
-	volatile REG_SET    brg;
-    }UART_REGS;
+typedef struct
+{
+    volatile REG_SET    mode;
+    volatile REG_SET    sta;
+    volatile REG_SET    tx;
+    volatile REG_SET    rx;
+    volatile REG_SET    brg;
+}UART_REGS;
 
 /*******************************************************************************
  * Enumerations
  ******************************************************************************/
 
-    typedef enum {
-        UART_1,
-        UART_2,
-        UART_3,
-        UART_4,
-        UART_5,
-        UART_6,
-        UART_INVALID,
-    } UART_ID;
+typedef enum {
+    UART_1,
+    UART_2,
+    UART_3,
+    UART_4,
+    UART_5,
+    UART_6,
+    UART_INVALID,
+} UART_ID;
 
-    typedef enum {
-        PARITY_EVEN,
-        PARITY_ODD,
-        PARITY_NONE,
-    } PARITY_type;
+typedef enum {
+    PARITY_EVEN,
+    PARITY_ODD,
+    PARITY_NONE,
+} PARITY_type;
 
 /*******************************************************************************
  * Public Data
@@ -73,17 +73,17 @@ extern UART_REGS * const UART[UART_PERIF_NUM];
  * Public Functions
  ******************************************************************************/
 
-    extern UART_ID  Uart_init( UART_ID id, int32_t baud, int32_t data_bits, PARITY_type parity, int32_t stop_bits );
+extern UART_ID  Uart_init( UART_ID id, int32_t baud, int32_t data_bits, PARITY_type parity, int32_t stop_bits );
 
-    extern void     Uart_enable( UART_ID id );
+extern void     Uart_enable( UART_ID id );
 
-    extern void     Uart_disable( UART_ID id );
+extern void     Uart_disable( UART_ID id );
 
-    extern int32_t      Uart_read( UART_ID id, int32_t byte_count, uint8_t* buffer, BOOL block );
+extern int32_t      Uart_read( UART_ID id, int32_t byte_count, uint8_t* buffer, BOOL block );
 
-    extern int32_t      Uart_write( UART_ID id, int32_t byte_count, uint8_t* buffer );
+extern int32_t      Uart_write( UART_ID id, int32_t byte_count, uint8_t* buffer );
 
-    extern void     Uart_check_baud( UART_ID id );
+extern void     Uart_check_baud( UART_ID id );
 
 
 #ifdef	__cplusplus
